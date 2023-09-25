61278842fe02789eafc9d0ce4c4a4cb80cee5c2a
GohChanKeong <88968473+GohChanKeong@users.noreply.github.com>
2023-09-25 17:32:10 +0800

    Update connect.php
    debug 2

--- student/connect.php
+++ student/connect.php
@@ -1,6 +1,6 @@
 <?php
         // Connect to your database (similar to what you've done in previous code)
-        $con = new mysqli('internshipdatabase.cyaehofy25iu.us-east-1.rds.amazonaws.com', 'admin', 'internsh1pDBPassw0rd', 'InternshipDatabase',3306);
+        $con = mysqli_connect('internshipdatabase.cyaehofy25iu.us-east-1.rds.amazonaws.com', 'admin', 'internsh1pDBPassw0rd', 'InternshipDatabase',3306);
                             // Check for connection errors
 
 ?>  
