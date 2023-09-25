8f60dcd3f76aabe80dcdb4d7be2f7edae45b57ba
GohChanKeong <88968473+GohChanKeong@users.noreply.github.com>
2023-09-25 16:12:51 +0800

    Update PreInternship.php
    edit endpoint

--- student/PreInternship.php
+++ student/PreInternship.php
@@ -67,7 +67,7 @@
                     $studID = 2205950; // Replace with the actual student ID
 
                     // Create a connection
-                    $con = new mysqli('localhost', 'root', '', 'internship');
+                    $con = new mysqli('internshipdatabase.cyaehofy25iu.us-east-1.rds.amazonaws.com', 'admin', 'internsh1pDBPassw0rd', 'InternshipDatabase');
 
                     // Check for connection errors
                     if ($con->connect_error) {
