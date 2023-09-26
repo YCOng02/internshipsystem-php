c06b399049e01854bdc63f1dee38da92c8ede17b
vincentchoows <vincentchoo88@gmail.com>
2023-09-26 12:40:21 +0800

    added local db string for testing

--- student/connect.php
+++ student/connect.php
@@ -2,7 +2,10 @@
         // Connect to your database (similar to what you've done in previous code)
         //$con = mysqli_connect('internshipdatabase.cyaehofy25iu.us-east-1.rds.amazonaws.com', 'admin', 'internsh1pDBPassw0rd', 'InternshipDatabase',3306);
                             // Check for connection errors
+                            
         $con = new mysqli('localhost', 'root', '', 'internship');
+
+        
         //for testing purposes----------------------------
         //$con = new mysqli('localhost', 'root', '', 'internship');
 
