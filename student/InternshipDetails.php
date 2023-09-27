2633496a6e8320c0bb34a1ef4897c668a3045ee7
OngYC02 <ongyc-pm20@student.tarc.edu.my>
2023-09-27 22:01:02 +0800

    staff restructure header and footer

--- student/InternshipDetails.php
+++ student/InternshipDetails.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Master Page</title>
     <meta charset="utf-8" />
@@ -11,6 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
+
 <body style="min-height:100vh;" class="bg-bright">
 
     <form id="form1" style="min-height:100vh;" runat="server">
@@ -39,23 +41,24 @@
                 <a href="Progress.php" class="nav-link col-4 text-black text-center" style="border-color: #FFFBD6">
                     Progress Report
                 </a>
-                <a href="InternshipDetails.php" class="nav-link active col-4 text-white border-0 text-center" style="background-color: #dc143c">
+                <a href="InternshipDetails.php" class="nav-link active col-4 text-white border-0 text-center"
+                    style="background-color: #dc143c">
                     Internship Details
                 </a>
             </div>
         </div>
 
-        
-        <div style="overflow-x: scroll; height:60vh;" class="container row justify-content-md-center mx-auto">   
-            <div class="container">
-            
-                <?php
-                    require 'connect.php';
-                    // Define the studentID (you need to retrieve this based on the logged-in student)
-                    $studID = 2205950; // Replace with the actual student ID
 
-                    // SQL query to retrieve internship details
-                    $internshipSql = "SELECT 
+        <div style="overflow-x: scroll; height:60vh;" class="container row justify-content-md-center mx-auto">
+            <div class="container">
+
+                <?php
+                require 'connect.php';
+                // Define the studentID (you need to retrieve this based on the logged-in student)
+                $studID = 2205950; // Replace with the actual student ID
+                
+                // SQL query to retrieve internship details
+                $internshipSql = "SELECT 
                         internship.internshipID, 
                         internship.studID, 
                         internship.internshipStatus, 
@@ -71,98 +74,98 @@
                     WHERE internship.studID = $studID";
 
 
-                    $internshipResult = $con->query($internshipSql);
+                $internshipResult = $con->query($internshipSql);
 
-                    
-                    if ($internshipResult->num_rows > 0) {
-                        echo '</br>';
-                        echo '</br>';
-                        echo '<div style="display: flex; justify-content: center;">';
-                        echo '<table style="width: 50%;" border="1">';
-                        echo '<tr style="background-color: lightgray; color: black; text-align: center; font-size: 25px; font-weight: bold;">';
-                        echo '<th style="width: 30%; "></th>';
-                        echo '<th style="width: 70%;text-align: left;">Internship Details</th>';
-                        echo '</tr>';
-                        
-                        while ($row = $internshipResult->fetch_assoc()) {
-                            echo "<tr><td>Internship ID:</td><td>" . $row["internshipID"] . "</td></tr>";
-                            echo "<tr><td>Student ID:</td><td>" . $row["studID"] . "</td></tr>";
-                            echo "<tr><td>Internship Status:</td><td>" . $row["internshipStatus"] . "</td></tr>";
-                            echo "<tr><td>Session ID:</td><td>" . $row["sessionID"] . "</td></tr>";
-                            echo "<tr><td>Start Month/Year:</td><td>" . $row["startMonthYear"] . "</td></tr>";
-                            echo "<tr><td>End Month/Year:</td><td>" . $row["endMonthYear"] . "</td></tr>";
-                            echo "<tr><td>Qualification:</td><td>" . $row["qualification"] . "</td></tr>";
-                            echo "<tr><td>Supervisor:</td><td>" . $row["supervisorName"] . "</td></tr>";
-                        }
-                    
-                        echo '</table>';
-                        echo '</div>'; // Close the centered div
-                    } else {
-                        echo "No internship details found for this student.";
+
+                if ($internshipResult->num_rows > 0) {
+                    echo '</br>';
+                    echo '</br>';
+                    echo '<div style="display: flex; justify-content: center;">';
+                    echo '<table style="width: 50%;" border="1">';
+                    echo '<tr style="background-color: lightgray; color: black; text-align: center; font-size: 25px; font-weight: bold;">';
+                    echo '<th style="width: 30%; "></th>';
+                    echo '<th style="width: 70%;text-align: left;">Internship Details</th>';
+                    echo '</tr>';
+
+                    while ($row = $internshipResult->fetch_assoc()) {
+                        echo "<tr><td>Internship ID:</td><td>" . $row["internshipID"] . "</td></tr>";
+                        echo "<tr><td>Student ID:</td><td>" . $row["studID"] . "</td></tr>";
+                        echo "<tr><td>Internship Status:</td><td>" . $row["internshipStatus"] . "</td></tr>";
+                        echo "<tr><td>Session ID:</td><td>" . $row["sessionID"] . "</td></tr>";
+                        echo "<tr><td>Start Month/Year:</td><td>" . $row["startMonthYear"] . "</td></tr>";
+                        echo "<tr><td>End Month/Year:</td><td>" . $row["endMonthYear"] . "</td></tr>";
+                        echo "<tr><td>Qualification:</td><td>" . $row["qualification"] . "</td></tr>";
+                        echo "<tr><td>Supervisor:</td><td>" . $row["supervisorName"] . "</td></tr>";
                     }
 
+                    echo '</table>';
+                    echo '</div>'; // Close the centered div
+                } else {
+                    echo "No internship details found for this student.";
+                }
 
 
-                    // SQL query to retrieve report names and grades from the internship table
-                    $reportSql = "SELECT * FROM internship WHERE studID = $studID";
 
-                    $reportResult = $con->query($reportSql);
+                // SQL query to retrieve report names and grades from the internship table
+                $reportSql = "SELECT * FROM internship WHERE studID = $studID";
 
-                    if ($reportResult->num_rows > 0) {
-                    
-                        // Output the retrieved report names and grades in a separate table
-                        echo '<br>';
-                        echo '<br>';
-                        echo '<div style="display: flex; justify-content: center;">';
-                        echo '<table style="width: 50%;" border="1">';
-                        echo '<tr style="background-color: lightgray; color: black; text-align: center; font-size: 25px; font-weight: bold;">';
-                        echo '<th style="width: 70%; text-align: center;">Report</th>';
-                        echo '<th style="width: 30%; text-align: left;">Grades</th>';
-                        echo '</tr>';
-                
-                        $reportFields = array(
-                            'monthlyReport1' => 'Monthly Report 1',
-                            'monthlyReport2' => 'Monthly Report 2',
-                            'monthlyReport3' => 'Monthly Report 3',
-                            'evaluationReport' => 'Evaluation Report'
-                        );
-                
-                        while ($row = $reportResult->fetch_assoc()) {
-                            foreach ($reportFields as $fieldName => $fieldLabel) {
-                                $grade = $row[$fieldName . 'Grade'];
-                                // Check if the grade is null and display as "Ungraded"
-                                if ($grade === null) {
-                                    $grade = "Ungraded";
-                                }
-                
-                                echo "<tr>
+                $reportResult = $con->query($reportSql);
+
+                if ($reportResult->num_rows > 0) {
+
+                    // Output the retrieved report names and grades in a separate table
+                    echo '<br>';
+                    echo '<br>';
+                    echo '<div style="display: flex; justify-content: center;">';
+                    echo '<table style="width: 50%;" border="1">';
+                    echo '<tr style="background-color: lightgray; color: black; text-align: center; font-size: 25px; font-weight: bold;">';
+                    echo '<th style="width: 70%; text-align: center;">Report</th>';
+                    echo '<th style="width: 30%; text-align: left;">Grades</th>';
+                    echo '</tr>';
+
+                    $reportFields = array(
+                        'monthlyReport1' => 'Monthly Report 1',
+                        'monthlyReport2' => 'Monthly Report 2',
+                        'monthlyReport3' => 'Monthly Report 3',
+                        'evaluationReport' => 'Evaluation Report'
+                    );
+
+                    while ($row = $reportResult->fetch_assoc()) {
+                        foreach ($reportFields as $fieldName => $fieldLabel) {
+                            $grade = $row[$fieldName . 'Grade'];
+                            // Check if the grade is null and display as "Ungraded"
+                            if ($grade === null) {
+                                $grade = "Ungraded";
+                            }
+
+                            echo "<tr>
                                         <td>$fieldLabel</td>
                                         <td>$grade</td>
                                     </tr>";
-                            }
-                            $finalGrade = $row['finalGrade'];
                         }
-                
-                        // Add the finalGrade to the report table as the last row
-                        // Check if the finalGrade is null and display as "Ungraded"
-                        if ($finalGrade === null) {
-                            $finalGrade = "Ungraded";
-                        }
-                        echo "<tr>
+                        $finalGrade = $row['finalGrade'];
+                    }
+
+                    // Add the finalGrade to the report table as the last row
+                    // Check if the finalGrade is null and display as "Ungraded"
+                    if ($finalGrade === null) {
+                        $finalGrade = "Ungraded";
+                    }
+                    echo "<tr>
                                 <td><b>Final Grade</b></td>
                                 <td><b> $finalGrade </b></td>
                             </tr>";
-                
-                        echo '</table>';
-                    } else {
-                        echo "No report grades found for this student.";
-                    }
-                
+
+                    echo '</table>';
+                } else {
+                    echo "No report grades found for this student.";
+                }
 
 
-                    // Close the database connection
-                    $con->close();
-                    ?>    
+
+                // Close the database connection
+                $con->close();
+                ?>
             </div>
         </div>
         </div>
@@ -184,7 +187,8 @@
         </footer>
     </form>
 
-    
-    
+
+
 </body>
+
 </html>
--- supervisor/SupervisorHome.php
+++ supervisor/SupervisorHome.php
@@ -1,5 +1,9 @@
+<!DOCTYPE html>
+<html>
 <?php
 session_start();
+$pageTitle = 'Home';
+include 'staffHeader.php';
 
 // Check if the session variables are set before accessing them
 if (isset($_SESSION['staffID'])) {
@@ -10,63 +14,23 @@ if (isset($_SESSION['staffID'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Home</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="css/style.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-
-</head>
-
-<body style="min-height:100vh" class="bg-bright">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
-        crossorigin="anonymous"></script>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="https://internship-bucket-23.s3.amazonaws.com/logo.png" width="250" height="80" />
-            </a>
-            <div class="navbar-container">
-                <div class="collapse navbar-collapse master" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="../Supervisor/SupervisorHome.php">Home <span
-                                    class="sr-only">(current)</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-    </nav>
-
-    <div class="container row justify-content-md-center mx-auto">
-        <div class="nav nav-tabs border-0" id="nav-tab" role="tablist">
-            <button id="btnCurrent" class="nav-link active w-50 text-white border-0"
-                style="background-color: #dc143c">Current</button>
-            <button id="btnUpcoming" class="nav-link w-50 text-black" style="border-color: #FFFBD6"
-                onclick="redirectToUpcoming()">Upcoming</button>
-        </div>
+<div class="container row justify-content-md-center mx-auto">
+    <div class="nav nav-tabs border-0" id="nav-tab" role="tablist">
+        <button id="btnCurrent" class="nav-link active w-50 text-white border-0"
+            style="background-color: #dc143c">Current</button>
+        <button id="btnUpcoming" class="nav-link w-50 text-black" style="border-color: #FFFBD6"
+            onclick="redirectToUpcoming()">Upcoming</button>
     </div>
+</div>
 
-    <script type="text/javascript">
-        function redirectToUpcoming() {
-            // Redirect to the "Upcoming.php" page when the button is clicked
-            window.location.href = "UpcomingInternship.php";
-        }
-    </script>
+<script type="text/javascript">
+    function redirectToUpcoming() {
+        // Redirect to the "Upcoming.php" page when the button is clicked
+        window.location.href = "UpcomingInternship.php";
+    }
+</script>
 
-    <div style="overflow-x: scroll;" class="container row justify-content-md-center mx-auto">
+<div style="overflow-x: scroll;" class="container row justify-content-md-center mx-auto">
     <div class="container" style="min-height:65vh; max-height:65vh;">
         <table id="StudentGV" class="table w-100 table-striped my-1 table-bordered table-responsive table-hover">
             <table class="table w-100 table-striped my-1 table-bordered table-responsive table-hover">
@@ -98,10 +62,8 @@ if (isset($_SESSION['staffID'])) {
                         AND Sup.staffID = Sta.staffID
                         AND (I.internshipStatus = 'In Progress' OR I.internshipStatus = 'Completed')
                         AND startMonthYear < '" . $currentDate . "'
-
-
-
                         AND endMonthYear > '" . $currentDate . "'
+                        AND  Sta.staffID = '" . $staffID . "'
                         ORDER BY Ses.sessionID";
 
                     $result = $con->query($sql);
@@ -142,20 +104,8 @@ if (isset($_SESSION['staffID'])) {
 
         </table>
     </div>
-    </div>
+</div>
 
-    <footer class="bg-light text-black pt-5 pb-4">
-        <div class="container text-center text-md-left">
-            <hr class="mb-4">
-            <div class="align-items-center">
-                <div class="col-md-12 col-lg-12 text-center">
-                    <p>Copyright © 2023 All Rights Reserved by:
-                        <strong>2023 - TAR UMT FOCS ITP</strong>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </footer>
-</body>
-
-</html>
+<?php
+include 'staffFooter.php';
+?>
