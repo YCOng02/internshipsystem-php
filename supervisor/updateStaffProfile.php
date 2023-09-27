fcec6af60ca350ce87ee227b5e564f2a0692374c
OngYC02 <ongyc-pm20@student.tarc.edu.my>
2023-09-27 21:00:04 +0800

    supervisor profile complete

--- supervisor/updateStaffProfile.php
+++ supervisor/updateStaffProfile.php
@@ -14,6 +14,9 @@ if (isset($_POST['action']) && $_POST['action'] == "ProfileUpdate") {
         die("Connection failed: " . $con->connect_error);
     }
 
+    $testName = "Ali"; //jane smith  
+
+
     $sql = "UPDATE staff SET staffName = ?, staffPhoneNo = ? WHERE staffID = ?";
     $stmt = $con->prepare($sql);
 
