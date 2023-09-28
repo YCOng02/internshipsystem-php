3ddfc02940ab2586fca2cd4dba1045153365160a
OngYC02 <ongyc-pm20@student.tarc.edu.my>
2023-09-28 09:38:34 +0800

    resolve two navigation bar problem

--- supervisor/SupervisorHome.php
+++ supervisor/SupervisorHome.php
@@ -17,43 +17,6 @@ if (isset($_SESSION['staffID'])) {
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title>Home</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="css/style.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-
-</head>
-
-<body style="min-height:100vh" class="bg-bright">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
-        crossorigin="anonymous"></script>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="https://gohchankeong-bucket.s3.amazonaws.com/logo.png" width="250" height="80" />
-            </a>
-            <div class="navbar-container">
-                <div class="collapse navbar-collapse master" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="../Supervisor/SupervisorHome.php">Home <span
-                                    class="sr-only">(current)</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-    </nav>
-
 <div class="container row justify-content-md-center mx-auto">
     <div class="nav nav-tabs border-0" id="nav-tab" role="tablist">
         <button id="btnCurrent" class="nav-link active w-50 text-white border-0"
