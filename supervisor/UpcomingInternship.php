f5e09176e94aa340f4534891deff9186a1cf1ba4
OngYC02 <ongyc-pm20@student.tarc.edu.my>
2023-09-28 18:03:03 +0800

    try resolving

--- supervisor/UpcomingInternship.php
+++ supervisor/UpcomingInternship.php
@@ -33,73 +33,71 @@ if (isset($_SESSION['staffID'])) {
 <div style="overflow-x: scroll;" class="container row justify-content-md-center mx-auto">
     <div class="container" style="min-height:65vh; max-height:65vh;">
         <table id="StudentGV" class="table w-100 table-striped my-1 table-bordered table-responsive table-hover">
-            <table class="table w-100 table-striped my-1 table-bordered table-responsive table-hover">
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone No</th>
-                    <th>Qualification</th>
-                    <th>Session</th>
-                </tr>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone No</th>
+                <th>Qualification</th>
+                <th>Session</th>
+            </tr>
 
-                <?php
-                // Define the current date
-                $currentDate = date('Y-m-d'); // Format: YYYY-MM-DD
-                
-                // Create a connection
-                require '../student/connect.php';
+            <?php
+            // Define the current date
+            $currentDate = date('Y-m-d'); // Format: YYYY-MM-DD
+            
+            // Create a connection
+            require '../student/connect.php';
 
-                // Check for connection errors
-                if ($con->connect_error) {
-                    throw new Exception("Connection failed: " . $con->connect_error);
-                } else {
-                    //generate the record in the table
-                    $sql = "SELECT Stud.studID, Stud.studName, Stud.studEmail, Stud.studPhoneNo, Stud.studQualification, Ses.sessionID, Sta.staffID, Sta.staffName
+            // Check for connection errors
+            if ($con->connect_error) {
+                throw new Exception("Connection failed: " . $con->connect_error);
+            } else {
+                //generate the record in the table
+                $sql = "SELECT Stud.studID, Stud.studName, Stud.studEmail, Stud.studPhoneNo, Stud.studQualification, Ses.sessionID, Sta.staffID, Sta.staffName
                         FROM student Stud, internship I, session Ses, supervisor Sup, staff Sta
                         WHERE Stud.studID = I.studID
                         AND I.sessionID = Ses.sessionID
                         AND Ses.sessionID = Sup.sessionID
                         AND Sup.staffID = Sta.staffID
                         AND (I.internshipStatus = 'In Progress' OR I.internshipStatus = 'Completed')
+                        AND  Sta.staffID = '" . $staffID . "'
                         AND startMonthYear > '" . $currentDate . "'
                         ORDER BY Ses.sessionID";
 
-                    $result = $con->query($sql);
+                $result = $con->query($sql);
 
-                    if ($result->num_rows > 0) {
+                if ($result->num_rows > 0) {
 
-                        while ($row = $result->fetch_assoc()) {
-                            echo '<tr style="cursor: pointer;" onclick="viewStudent(' . $row['studID'] . ')" data-href="StudentDetail.php?studID=' . $row['studID'] . '">';
-                            echo '<td>' . $row['studID'] . '</td>';
-                            echo '<td>' . $row['studName'] . '</td>';
-                            echo '<td>' . $row['studEmail'] . '</td>';
-                            echo '<td>' . $row['studPhoneNo'] . '</td>';
-                            echo '<td>' . $row['studQualification'] . '</td>';
-                            echo '<td>' . $row['sessionID'] . '</td>';
-                            echo '</tr>';
-                        }
-
-
-                    } else {
-                        echo '<tr>';
-                        echo '<td colspan="6" class="text-center"> No records found.</td>';
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<tr style="cursor: pointer;" onclick="viewStudent(' . $row['studID'] . ')" data-href="StudentDetail.php?studID=' . $row['studID'] . '">';
+                        echo '<td>' . $row['studID'] . '</td>';
+                        echo '<td>' . $row['studName'] . '</td>';
+                        echo '<td>' . $row['studEmail'] . '</td>';
+                        echo '<td>' . $row['studPhoneNo'] . '</td>';
+                        echo '<td>' . $row['studQualification'] . '</td>';
+                        echo '<td>' . $row['sessionID'] . '</td>';
                         echo '</tr>';
                     }
 
-                    $con->close();
+                } else {
+                    echo '<tr>';
+                    echo '<td colspan="6" class="text-center"> No records found.</td>';
+                    echo '</tr>';
                 }
 
+                $con->close();
+            }
 
-                ?>
-            </table>
-            <!--If the user click a row, it will be redirect to the student detail page-->
-            <script type="text/javascript">
-                function viewStudent(id) {
-                    // Perform a client-side redirection to the StudentDetail.aspx page with the extracted ID
-                    window.location.href = "/StudentDetail.php?StudID=" + encodeURIComponent(id);
-                }
-            </script>
+            ?>
+        </table>
+        <!--If the user click a row, it will be redirect to the student detail page-->
+        <script type="text/javascript">
+            function viewStudent(id) {
+                // Perform a client-side redirection to the StudentDetail.aspx page with the extracted ID
+                window.location.href = "StudentDetail.php?StudID=" + encodeURIComponent(id);
+            }
+        </script>
 
         </table>
     </div>
